0e34a7c2441511257dda94a79a6791401f170f52
Thomas Kroeber <thomas.kroeber@here.com>
2013-10-28 18:21:52 +0100

    new includes for new nvd3

--- vendor/assets/javascripts/nvd3-rails.js
+++ vendor/assets/javascripts/nvd3-rails.js
@@ -1,7 +1,8 @@
-//= require ./nvd3/lib/d3.v2
+//= require ./nvd3/lib/d3.v3
 //= require ./nvd3/lib/fisheye
 //= require ./nvd3/src/core
 //= require ./nvd3/src/tooltip
 //= require ./nvd3/src/utils.js
+//= require ./nvd3/src/interactiveLayer
 //= require_tree ./nvd3/src/models
 //= require set-nvd3-env.js.erb
